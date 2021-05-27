0e50beaeb55ab718a864c0b4c8d7ae702f19ad62
Kai Bonsol <kaibonsol@gmail.com>
2021-05-26 21:10:20 -0500

    added links to navbar

--- client/src/components/utilities/Navbar.js
+++ client/src/components/utilities/Navbar.js
@@ -7,11 +7,11 @@ export default function Navbar() {
     return (
         <div className='navbar'>
             <ul>
-                <NavbarButton imgUrl="contactlogo.png" link="/" name="Contact"/>
-                <NavbarButton imgUrl="devlogo.png" link="/" name="DevPost"/>
-                <NavbarButton imgUrl="gitlogo.png" link="/" name="GitHub"/>
-                <NavbarButton imgUrl="linkedinlogo.png" link="/" name="LinkedIn"/>
-                <NavbarButton imgUrl="spotifylogo.png" link="/" name="Spotify"/>
+                {/* <NavbarButton imgUrl="contactlogo.png" link="" name="Contact"/> */}
+                <NavbarButton imgUrl="devlogo.png" link="https://devpost.com/kaibonsol" name="DevPost"/>
+                <NavbarButton imgUrl="gitlogo.png" link="https://github.com/kaibonsol" name="GitHub"/>
+                <NavbarButton imgUrl="linkedinlogo.png" link="https://www.linkedin.com/in/kai-bonsol/" name="LinkedIn"/>
+                <NavbarButton imgUrl="spotifylogo.png" link="https://open.spotify.com/user/12134345729?si=f890f6551cc5410e" name="Spotify"/>
             </ul>
         </div>
     )
