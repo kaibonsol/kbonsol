581cc51f995cd24c02c84a56f003086dc22fb4c2
Kai Bonsol <kaibonsol@gmail.com>
2021-05-26 21:18:28 -0500

    logo txt outline, small deploy changes

--- client/src/components/Body.js
+++ client/src/components/Body.js
@@ -6,6 +6,7 @@ export default function Body() {
     return (
         <div className="body">
             <h1>Welcome to my website!</h1>
+            <h2>Currently under construction :)</h2>
         </div>
     )
 }
--- client/src/components/utilities/NavbarButton.js
+++ client/src/components/utilities/NavbarButton.js
@@ -3,6 +3,6 @@ import React from 'react'
 export default function NavbarButton(props) {
 
     return (
-        <li><a href={props.link}><img className='img-ico' alt={props.name} src={props.imgUrl}/></a></li>
+        <li><a href={props.link} rel='noreferrer' target='_blank'><img className='img-ico' alt={props.name} src={props.imgUrl}/></a></li>
     )
 }
