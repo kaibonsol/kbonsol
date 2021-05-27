9bd76d5c569677fc033578e918bac60fa702cb2c
Kai Bonsol <kaibonsol@gmail.com>
2021-05-26 21:06:41 -0500

    added linkedin, edit styles

--- client/src/components/utilities/Navbar.js
+++ client/src/components/utilities/Navbar.js
@@ -10,6 +10,7 @@ export default function Navbar() {
                 <NavbarButton imgUrl="contactlogo.png" link="/" name="Contact"/>
                 <NavbarButton imgUrl="devlogo.png" link="/" name="DevPost"/>
                 <NavbarButton imgUrl="gitlogo.png" link="/" name="GitHub"/>
+                <NavbarButton imgUrl="linkedinlogo.png" link="/" name="LinkedIn"/>
                 <NavbarButton imgUrl="spotifylogo.png" link="/" name="Spotify"/>
             </ul>
         </div>
