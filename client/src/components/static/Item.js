3490c9ef82a9b82a8c5d6438564e48181b6e195a
Kai Bonsol <kaibonsol@gmail.com>
2021-06-13 12:39:27 -0500

    conditional style for item.js, css for panel fade

--- client/src/components/static/Item.js
+++ client/src/components/static/Item.js
@@ -7,8 +7,12 @@ function Item(props) {
     const data = props.data
     const src = data.src && <External to={data.src} text="Source" />
     const desc = data.desc && <div className="desc">{props.data.desc}. <div>{src}</div></div> 
+
+    // if tbl is set then set li style to display:inline-block
+    const style = data.tbl ? {display: "inline-block", marginRight:"10%", marginBottom:"3%"} : {display: "block"}
+
     return (
-        <li>
+        <li style={style}>
             <div className="name">{data.name}</div>
             {desc}
         </li>
