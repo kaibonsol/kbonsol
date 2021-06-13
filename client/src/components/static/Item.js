fe5a541bd80d11d512354c2d9117086dfcbe8358
Kai Bonsol <kaibonsol@gmail.com>
2021-06-13 16:35:13 -0500

    mobile UI improvement

--- client/src/components/static/Item.js
+++ client/src/components/static/Item.js
@@ -4,12 +4,43 @@ import External from '../utilities/External'
 import './Item.css'
 
 function Item(props) {
+    
+    const [dimensions, setDimensions] = React.useState({ 
+        height: window.innerHeight,
+        width: window.innerWidth
+    })
+
+    React.useEffect(() => {
+
+        function handleResize() {
+            setDimensions({
+                height: window.innerHeight,
+                width: window.innerWidth
+            })
+        
+        }
+
+        // add and remove resize event listener
+
+        window.addEventListener('resize', handleResize)
+
+        return _ => {
+            window.removeEventListener('resize', handleResize)
+        }
+
+    })
+
+
     const data = props.data
     const src = data.src && <External to={data.src} text="Source" />
     const desc = data.desc && <div className="desc">{props.data.desc}. <div>{src}</div></div> 
 
     // if tbl is set then set li style to display:inline-block
     const style = data.tbl ? {display: "inline-block", marginRight:"10%", marginBottom:"5%"} : {display: "block"}
+    if(dimensions.width < 528)
+    {
+        style["marginLeft"] = "-10%"
+    }
 
     return (
         <li style={style}>
